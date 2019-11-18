7a465997637dfcaebdd7909cb610c3cac3b7f984
Maddy <mmcintosh2k14@gmail.com>
2019-11-18 14:08:14 -0500

    adds reducer boilerplate, necessary dependencies and cleans up index/app.js files.

---
+++ oer-bookr/src/reducers/index.js
@@ -0,0 +1,17 @@
+// action type imports
+
+
+const initialState = {
+    books: [],
+    error: null,
+    isFetching: false
+};
+
+function reducer(state = initialState, action){
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
+
+export default reducer
