8a2a98032a636822c09cb6868a1cca1a314174e3
Maddy <mmcintosh2k14@gmail.com>
2019-11-20 21:16:01 -0500

    completes signup post functionality

--- oer-bookr/src/components/Signup.js
+++ oer-bookr/src/components/Signup.js
@@ -1,45 +1,45 @@
 import React, {useState} from "react";
 import {BrowserRouter as Router, Link} from 'react-router-dom';
+import axios from 'axios';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
+
 const SignUp = props => {
     const [signUp, setSignUp] = useState({
         username: "",
-        password: "",
-        email: "",
-        firstname:"",
-        lastname:""
+        password: ""
     })
     const handleChanges = event => {
         setSignUp({...signUp, [event.target.name]: event.target.value})
+        console.log(signUp)
+    }
+
+    const register = e => {
+        e.preventDefault()
+        const user = {
+            username: signUp.username,
+            password: signUp.password
+        }
+        axios
+        .post('https://oerbookr.herokuapp.com/api/auth/registration', user)
+        .then(res => console.log(res.data))
+        .catch(err => console.log('Login Error', err.res.message))
     }
 
     return (
         <>
-            <form className='signup'>
-                <label htmlFor='firstname'>
-                    <div>First Name</div> 
-                    <TextField onChange={handleChanges} id='firstname' type='text'></TextField>
-                </label>
-                <label htmlFor='lastname'>
-                    <div>Last Name</div>
-                    <TextField id='lastname' type='text' onChange={handleChanges}></TextField>  
-                </label>
+            <form className='signup' onSubmit={register}>
                 <label htmlFor='username'>
                     <div>Username</div> 
-                    <TextField onChange={handleChanges} id='username' type='text'></TextField>
+                    <TextField onChange={handleChanges} name='username' id='username' type='text' />
                 </label>
                 <label htmlFor='password'>
                     <div>Password</div>
-                    <TextField id='password' type='text' onChange={handleChanges}></TextField>  
-                </label>
-                <label htmlFor='email'>
-                    <div>Email</div>
-                    <TextField id='email' type='text' onChange={handleChanges}></TextField>
+                    <TextField id='password' name='password' type='text' onChange={handleChanges} /> 
                 </label>
                 <br/>
                 <Link className='loginPage' style={{textDecoration: 'none'}} to='/'><Button>Login</Button></Link>
-                <Button>Sign Up</Button>
+                <Button type='submit'>Sign Up</Button>
             </form>
             
             
